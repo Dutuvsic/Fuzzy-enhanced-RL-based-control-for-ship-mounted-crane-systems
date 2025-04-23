60b658e5c8109503c1faf30f095e0d1447ee0c2c
Dutuvsic <57703930+Dutuvsic@users.noreply.github.com>
2025-04-23 12:11:45 +0800

    Update ndi2lin.m

--- ndi2lin.m
+++ ndi2lin.m
@@ -1,3 +1,3 @@
 function lin = ndi2lin(ndi,dims)
-c = [1 cumprod(dims(1:length(dims)-1))];    % vector of "bases"
+c = [1 cumprod(dims(1:length(dims)-1))];    % vector of bases
 lin = (c*(ndi-1)')' + 1;                    % compute lin. index
